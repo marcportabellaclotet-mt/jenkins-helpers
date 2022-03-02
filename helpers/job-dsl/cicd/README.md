c4d1ba5ff6542abf945417c31372e07e546f753e
Marc Portabella <marcportabellaclotet@gmail.com>
2022-03-02 11:10:31 +0100

    feat: add cicd helper

---
+++ helpers/job-dsl/cicd/README.md
@@ -0,0 +1,75 @@
+# Jenkins CICD Helper
+---
+## Table of Contents
+
+- [Description](#description)
+
+- [Requirements](#requirements)
+
+- [Usage](#usage)
+
+---
+## Description
+
+**CICD helpers** provide code-based pipelines to build and deploy apps.
+Jenkins folders definition.
+
+Jenkins folders and their permissions are defined in YAML files hosted in your code repository.
+
+## Requirements
+
+To use folder helper, some plugins needs to be installed:
+- [job-dsl](https://plugins.jenkins.io/job-dsl/)
+- [matrix-auth](https://plugins.jenkins.io/matrix-auth/)
+- [folders](https://plugins.jenkins.io/cloudbees-folder/)
+- [folder-properties](https://plugins.jenkins.io/folder-properties/)
+
+Seed job-dsl needs to be [configured](https://www.digitalocean.com/community/tutorials/how-to-automate-jenkins-job-configuration-using-job-dsl).
+
+Project-based Matrix Authorization Strategy needs to be configured in Jenkins "Configure Global Security"
+
+A code repository to host configuration files
+
+## Usage
+
+Point your seed job to read the [folders.groovy](folders.groovy) file
+
+<details><summary>Define main folder configuration</summary>
+
+```
+
+  config:
+    main_folder: "services" # Name of the main folder.
+    main_folder_display_name: "Services" # Name of the main folder which will be displayed in Jenkins UI.
+    main_folder_description: "Services" # Name of the main folder which will be displayed in Jenkins UI.
+    allow_all_auth_users: true # Allow all authenticated Jenkins users to view the main folder.
+    allowed_users:
+      - allowed-user # List of Jenkins users allowed to view the main folder.
+    allowed_groups:
+      - allowed-group # List of Jenkins groups allowed to view the main folder.
+
+```
+</details>
+
+<details><summary>Define your service folders in yaml files</summary>
+
+```
+
+  example-service: #name of the service folder which will be displayed under the main folder.
+    owner: user-owner # Name of the user who owns the service (optional).
+    group_owner: group-owner # # Name of the user who owns the service (optional).
+    allowed_users: # List of additional users with full permissions.
+      - allowed-user
+    allowed_ro_users: # List of additional users with read permissions.
+      - allowed-ro-user
+    allowed_groups: # List of additional groups with full permissions.
+      - allowed-group
+    folder_properties: # Folder properties that will be added to your service folders.
+      SAMPLE_PROPERTY: 'some value'
+
+```
+</details>
+
+Folder helper tries to find configuration files in helpers/job-dsl/folders/files/${JENKINS_SERVER_NAME}.
+
+This value can be overridden by setting **folders_config_path** environment variable in Jenkins global parameters.
---
+++ helpers/job-dsl/cicd/cicd.groovy
@@ -0,0 +1,96 @@
+@Grab('org.yaml:snakeyaml:1.17')
+import org.yaml.snakeyaml.Yaml
+import groovy.io.FileType
+import hudson.FilePath
+import jenkins.model.Jenkins
+
+instance = Jenkins.getInstance()
+globalNodeProperties = instance.getGlobalNodeProperties()
+envVarsNodePropertyList = globalNodeProperties.getAll(hudson.slaves.EnvironmentVariablesNodeProperty.class)
+envVars = envVarsNodePropertyList.get(0).getEnvVars()
+
+JENKINS_SERVER_NAME = InetAddress.localHost.canonicalHostName
+CICD_CONFIG_PATH = envVars['cicd_config_path']
+if (! CICD_CONFIG_PATH.find()){
+  println "------ INFO ------"
+  println "Enviroment variable [cicd_config_path] not defined."
+  println "You can define this value in jenkins global properties | environment variables"
+  println "Using default value (helpers/job-dsl/services/folders/files/${JENKINS_SERVER_NAME}/)."
+  println "------------------"
+  CICD_YAML_PATH = "helpers/job-dsl/cicd/files/${JENKINS_SERVER_NAME}/"
+} else {
+  CICD_YAML_PATH = "${CICD_CONFIG_PATH}/"
+  println "Using Service Folders Yaml Path : ${CICD_YAML_PATH}/"
+}
+
+CICD_CONFIG_PATH = "${CICD_YAML_PATH}cicd.config"
+CICD_CONFIG = new Yaml().load(readFileFromWorkspace(CICD_CONFIG_PATH))
+
+MAIN_FOLDER      = CICD_CONFIG.config.main_folder
+CICD_FOLDER_NAME = CICD_CONFIG.config.cicd_folder
+
+hudson.FilePath workspace = hudson.model.Executor.currentExecutor().getCurrentWorkspace()
+cwd = hudson.model.Executor.currentExecutor().getCurrentWorkspace().absolutize()
+yamlFiles = new FilePath(cwd, CICD_YAML_PATH).list('*.yaml')
+yamlFiles.each { file ->
+  servicesList = new Yaml().load(readFileFromWorkspace(file.getRemote()))
+  servicesList.each{serviceName, serviceConfig->
+    def servicesFolderProperties = serviceConfig.folder_properties
+    def resourceList = serviceConfig.resources
+    def deploymentsFolderJob = MAIN_FOLDER + '/' +
+                                serviceName + '/' +
+                                CICD_FOLDER_NAME
+    folder(deploymentsFolderJob) {
+      displayName(CICD_FOLDER_NAME)
+      description(CICD_FOLDER_NAME)
+      properties {
+        folderProperties {
+          properties {
+            servicesFolderProperties.each {element->
+              stringProperty {
+                key(element.key)
+                value(element.value)
+              }
+            }
+          }
+        }
+      }
+    }
+    def resourceList = serviceConfig.resources
+    resourceList.each{resourceName,resourceConfig->
+      def resourcesFolderProperties = resourceConfig.folder_properties
+      def resourceNameFolderJob = MAIN_FOLDER + '/' +
+                                  serviceName + '/' +
+                                  CICD_FOLDER_NAME + '/' +
+                                  resourceName
+
+      def createBuildJob = resourceConfig.create_build_job
+      def buildServiceRepoURL = resourceConfig.build_service_repo
+      def buildJobJenkinsfilePath = resourceConfig.jenkinsfile_build_path
+      def buildJobJenkinsfileBranch = resourceConfig.jenkinsfile_build_branch
+
+      def deployServiceRepoURL = resourceConfig.deploy_service_repo
+      def deployJobJenkinsfilePath = resourceConfig.jenkinsfile_deploy_path
+      def deployJobJenkinsfileBranch = resourceConfig.jenkinsfile_deploy_branch
+      def createDeployJob = resourceConfig.create_deploy_job
+
+      folder(resourceNameFolderJob) {
+        displayName(resourceName)
+        description(resourceName)
+        properties {
+          folderProperties {
+            properties {
+              resourcesFolderProperties.each {element->
+                stringProperty {
+                  key(element.key)
+                  value(element.value)
+                }
+              }
+            }
+          }
+        }
+      }
+
+    }
+  }
+}

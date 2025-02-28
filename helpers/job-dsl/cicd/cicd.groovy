0a7d877c75a46be57c0108499f06a03f4efce2f5
Marc Portabella Clotet <marcportabellaclotet@gmail.com>
2025-03-01 00:44:19 +0100

    Update cicd.groovy

--- helpers/job-dsl/cicd/cicd.groovy
+++ helpers/job-dsl/cicd/cicd.groovy
@@ -4,25 +4,26 @@ import groovy.io.FileType
 import hudson.FilePath
 import jenkins.model.Jenkins
 
-instance = Jenkins.getInstance()
-globalNodeProperties = instance.getGlobalNodeProperties()
-envVarsNodePropertyList = globalNodeProperties.getAll(hudson.slaves.EnvironmentVariablesNodeProperty.class)
-envVars = envVarsNodePropertyList.get(0).getEnvVars()
+//instance = Jenkins.getInstance()
+//globalNodeProperties = instance.getGlobalNodeProperties()
+//envVarsNodePropertyList = globalNodeProperties.getAll(hudson.slaves.EnvironmentVariablesNodeProperty.class)
+//envVars = envVarsNodePropertyList.get(0).getEnvVars()
 
-JENKINS_SERVER_NAME = InetAddress.localHost.canonicalHostName
-CICD_CONFIG_PATH = envVars['cicd_config_path']
-if (! CICD_CONFIG_PATH.find()){
-  println "------ INFO ------"
-  println "Enviroment variable [cicd_config_path] not defined."
-  println "You can define this value in jenkins global properties | environment variables"
-  println "Using default value (helpers/job-dsl/services/folders/files/${JENKINS_SERVER_NAME}/)."
-  println "------------------"
-  CICD_YAML_PATH = "helpers/job-dsl/cicd/files/${JENKINS_SERVER_NAME}/"
-} else {
-  CICD_YAML_PATH = "${CICD_CONFIG_PATH}/"
-  println "Using Service Folders Yaml Path : ${CICD_YAML_PATH}/"
-}
+//JENKINS_SERVER_NAME = InetAddress.localHost.canonicalHostName
+//CICD_CONFIG_PATH = envVars['cicd_config_path']
+//if (! CICD_CONFIG_PATH.find()){
+//  println "------ INFO ------"
+//  println "Enviroment variable [cicd_config_path] not defined."
+//  println "You can define this value in jenkins global properties | environment variables"
+//  println "Using default value (helpers/job-dsl/services/folders/files/${JENKINS_SERVER_NAME}/)."
+//  println "------------------"
+//  CICD_YAML_PATH = "helpers/job-dsl/cicd/files/${JENKINS_SERVER_NAME}/"
+//} else {
+//  CICD_YAML_PATH = "${CICD_CONFIG_PATH}/"
+//  println "Using Service Folders Yaml Path : ${CICD_YAML_PATH}/"
+//}
 
+CICD_YAML_PATH = "helpers/job-dsl/cicd/files/jenkins-example/"
 CICD_CONFIG_PATH = "${CICD_YAML_PATH}cicd.config"
 CICD_CONFIG = new Yaml().load(readFileFromWorkspace(CICD_CONFIG_PATH))
 
